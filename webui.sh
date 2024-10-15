f2d2c2de7065a218eaa6d6a43d64e8e2887a3008
Crivellaro Diego <crivellarodiego.82@gmail.com>
2024-10-15 08:57:37 +0200

    webui.sh

--- webui.sh
+++ webui.sh
@@ -1,14 +1,15 @@
 #!/usr/bin/env bash
 
+# Utilizza l'ambiente virtuale
 use_venv=1
 if [[ $venv_dir == "-" ]]; then
   use_venv=0
 fi
 
+# Imposta la directory dello script
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
-
-# If run from macOS, load defaults from webui-macos-env.sh
+# Se eseguito da macOS, carica le impostazioni predefinite da webui-macos-env.sh
 if [[ "$OSTYPE" == "darwin"* ]]; then
     if [[ -f "$SCRIPT_DIR"/webui-macos-env.sh ]]
         then
@@ -16,53 +17,54 @@ if [[ "$OSTYPE" == "darwin"* ]]; then
     fi
 fi
 
-# Read variables from webui-user.sh
+# Leggi le variabili da webui-user.sh
 # shellcheck source=/dev/null
 if [[ -f "$SCRIPT_DIR"/webui-user.sh ]]
 then
     source "$SCRIPT_DIR"/webui-user.sh
 fi
 
-# Set defaults
-# Install directory without trailing slash
+# Imposta le variabili predefinite
+# Directory di installazione senza barra finale
 if [[ -z "${install_dir}" ]]
 then
     install_dir="$SCRIPT_DIR"
 fi
 
-# Name of the subdirectory (defaults to stable-diffusion-webui)
+# Nome della sottodirectory (predefinita su stable-diffusion-webui)
 if [[ -z "${clone_dir}" ]]
 then
     clone_dir="stable-diffusion-webui"
 fi
 
-# python3 executable
+# Eseguibile di python3
 if [[ -z "${python_cmd}" ]]
 then
     python_cmd="python3"
 fi
 
-# git executable
+# Eseguibile di git
 if [[ -z "${GIT}" ]]
 then
     export GIT="git"
 fi
 
-# python3 venv without trailing slash (defaults to ${install_dir}/${clone_dir}/venv)
+# Ambiente virtuale di python3 senza barra finale (predefinito su ${install_dir}/${clone_dir}/venv)
 if [[ -z "${venv_dir}" ]] && [[ $use_venv -eq 1 ]]
 then
     venv_dir="venv"
 fi
 
+# Se non specificato, lo script da lanciare è prepare.py
 if [[ -z "${LAUNCH_SCRIPT}" ]]
 then
     LAUNCH_SCRIPT="prepare.py"
 fi
 
-# this script cannot be run as root by default
+# Questo script non può essere eseguito come root per impostazione predefinita
 can_run_as_root=0
 
-# read any command line flags to the webui.sh script
+# Leggi eventuali flag dalla riga di comando allo script webui.sh
 while getopts "f" flag > /dev/null 2>&1
 do
     case ${flag} in
@@ -71,48 +73,53 @@ do
     esac
 done
 
+# Disabilita il logging di Sentry
 export ERROR_REPORTING=FALSE
 
+# Non reinstallare pacchetti pip esistenti su Debian/Ubuntu
 export PIP_IGNORE_INSTALLED=0
 
+# Stampa formattata
 delimiter="################################################################"
 
 printf "\n%s\n" "${delimiter}"
-printf "\e[1m\e[32mInstall script for stable-diffusion + Web UI\n"
-printf "\e[1m\e[34mTested on Debian 11 (Bullseye)\e[0m"
+printf "\e[1m\e[32mScript di installazione per stable-diffusion + Web UI\n"
+printf "\e[1m\e[34mTestato su Debian 11 (Bullseye)\e[0m"
 printf "\n%s\n" "${delimiter}"
 
-# Do not run as root
+# Non eseguire come root
 if [[ $(id -u) -eq 0 && can_run_as_root -eq 0 ]]
 then
     printf "\n%s\n" "${delimiter}"
-    printf "\e[1m\e[31mERROR: This script must not be launched as root, aborting...\e[0m"
+    printf "\e[1m\e[31mERRORE: Questo script non deve essere avviato come root, abortendo...\e[0m"
     printf "\n%s\n" "${delimiter}"
     exit 1
 else
     printf "\n%s\n" "${delimiter}"
-    printf "Running on \e[1m\e[32m%s\e[0m user" "$(whoami)"
+    printf "Eseguendo come utente \e[1m\e[32m%s\e[0m" "$(whoami)"
     printf "\n%s\n" "${delimiter}"
 fi
 
+# Controllo se si sta eseguendo su un sistema operativo a 32 bit
 if [[ $(getconf LONG_BIT) = 32 ]]
 then
     printf "\n%s\n" "${delimiter}"
-    printf "\e[1m\e[31mERROR: Unsupported Running on a 32bit OS\e[0m"
+    printf "\e[1m\e[31mERRORE: Esecuzione su un sistema operativo a 32 bit non supportata\e[0m"
     printf "\n%s\n" "${delimiter}"
     exit 1
 fi
 
+# Controlla se il repository è già stato clonato
 if [[ -d .git ]]
 then
     printf "\n%s\n" "${delimiter}"
-    printf "Repo already cloned, using it as install directory"
+    printf "Repository già clonato, usando come directory di installazione"
     printf "\n%s\n" "${delimiter}"
     install_dir="${PWD}/../"
     clone_dir="${PWD##*/}"
 fi
 
-# Check prerequisites
+# Controlla i prerequisiti
 gpu_info=$(lspci 2>/dev/null | grep -E "VGA|Display")
 case "$gpu_info" in
     *"Navi 1"*)
@@ -122,10 +129,10 @@ case "$gpu_info" in
             pyv="$(${python_cmd} -c 'import sys; print(".".join(map(str, sys.version_info[0:2])))')"
             if [[ $(bc <<< "$pyv <= 3.10") -eq 1 ]] 
             then
-                # Navi users will still use torch 1.13 because 2.0 does not seem to work.
+                # Gli utenti Navi utilizzeranno ancora torch 1.13 perché 2.0 non sembra funzionare.
                 export TORCH_COMMAND="pip install torch==1.13.1+rocm5.2 torchvision==0.14.1+rocm5.2 --index-url https://download.pytorch.org/whl/rocm5.2"
             else
-                printf "\e[1m\e[31mERROR: RX 5000 series GPUs must be using at max python 3.10, aborting...\e[0m"
+                printf "\e[1m\e[31mERRORE: Le GPU della serie RX 5000 devono utilizzare al massimo python 3.10, abortendo...\e[0m"
                 exit 1
             fi
         fi
@@ -134,12 +141,10 @@ case "$gpu_info" in
     ;;
     *"Navi 3"*) [[ -z "${TORCH_COMMAND}" ]] && \
          export TORCH_COMMAND="pip install --pre torch torchvision --index-url https://download.pytorch.org/whl/nightly/rocm5.6"
-        # Navi 3 needs at least 5.5 which is only on the nightly chain, previous versions are no longer online (torch==2.1.0.dev-20230614+rocm5.5 torchvision==0.16.0.dev-20230614+rocm5.5 torchaudio==2.1.0.dev-20230614+rocm5.5)
-        # so switch to nightly rocm5.6 without explicit versions this time
     ;;
     *"Renoir"*) export HSA_OVERRIDE_GFX_VERSION=9.0.0
         printf "\n%s\n" "${delimiter}"
-        printf "Experimental support for Renoir: make sure to have at least 4GB of VRAM and 10GB of RAM or enable cpu mode: --use-cpu all --no-half"
+        printf "Supporto sperimentale per Renoir: assicurati di avere almeno 4GB di VRAM e 10GB di RAM oppure abilita la modalità CPU: --use-cpu all --no-half"
         printf "\n%s\n" "${delimiter}"
     ;;
     *)
@@ -153,96 +158,78 @@ then
     fi
 fi
 
+# Controlla se i prerequisiti sono installati
 for preq in "${GIT}" "${python_cmd}"
 do
     if ! hash "${preq}" &>/dev/null
     then
         printf "\n%s\n" "${delimiter}"
-        printf "\e[1m\e[31mERROR: %s is not installed, aborting...\e[0m" "${preq}"
+        printf "\e[1m\e[31mERRORE: %s non è installato, abortendo...\e[0m" "${preq}"
         printf "\n%s\n" "${delimiter}"
         exit 1
     fi
 done
 
+# Controlla se l'ambiente virtuale è disponibile
 if [[ $use_venv -eq 1 ]] && ! "${python_cmd}" -c "import venv" &>/dev/null
 then
     printf "\n%s\n" "${delimiter}"
-    printf "\e[1m\e[31mERROR: python3-venv is not installed, aborting...\e[0m"
+    printf "\e[1m\e[31mERRORE: python3-venv non è installato, abortendo...\e[0m"
     printf "\n%s\n" "${delimiter}"
     exit 1
 fi
 
-cd "${install_dir}"/ || { printf "\e[1m\e[31mERROR: Can't cd to %s/, aborting...\e[0m" "${install_dir}"; exit 1; }
+# Cambia directory di lavoro
+cd "${install_dir}"/ || { printf "\e[1m\e[31mERRORE: Impossibile accedere a %s/, abortendo...\e[0m" "${install_dir}"; exit 1; }
 if [[ -d "${clone_dir}" ]]
 then
-    cd "${clone_dir}"/ || { printf "\e[1m\e[31mERROR: Can't cd to %s/%s/, aborting...\e[0m" "${install_dir}" "${clone_dir}"; exit 1; }
+    cd "${clone_dir}"/ || { printf "\e[1m\e[31mERRORE: Impossibile accedere a %s/%s/, abortando...\e[0m" "${install_dir}" "${clone_dir}"; exit 1; }
 else
     printf "\n%s\n" "${delimiter}"
-    printf "Clone stable-diffusion-webui"
+    printf "Clonare stable-diffusion-webui"
     printf "\n%s\n" "${delimiter}"
     "${GIT}" clone https://github.com/AUTOMATIC1111/stable-diffusion-webui.git "${clone_dir}"
-    cd "${clone_dir}"/ || { printf "\e[1m\e[31mERROR: Can't cd to %s/%s/, aborting...\e[0m" "${install_dir}" "${clone_dir}"; exit 1; }
+    cd "${clone_dir}"/ || { printf "\e[1m\e[31mERRORE: Impossibile accedere a %s/%s/, abortando...\e[0m" "${install_dir}" "${clone_dir}"; exit 1; }
     cp ../prepare.py .
 fi
 
+# Creare e attivare un ambiente virtuale python
 if [[ $use_venv -eq 1 ]] && [[ -z "${VIRTUAL_ENV}" ]];
 then
     printf "\n%s\n" "${delimiter}"
-    printf "Create and activate python venv"
+    printf "Creando un ambiente virtuale python in %s" "${venv_dir}"
     printf "\n%s\n" "${delimiter}"
-    cd "${install_dir}"/"${clone_dir}"/ || { printf "\e[1m\e[31mERROR: Can't cd to %s/%s/, aborting...\e[0m" "${install_dir}" "${clone_dir}"; exit 1; }
-    if [[ ! -d "${venv_dir}" ]]
-    then
-        "${python_cmd}" -m venv "${venv_dir}"
-        first_launch=1
-    fi
-    # shellcheck source=/dev/null
-    if [[ -f "${venv_dir}"/bin/activate ]]
-    then
-        source "${venv_dir}"/bin/activate
-    else
-        printf "\n%s\n" "${delimiter}"
-        printf "\e[1m\e[31mERROR: Cannot activate python venv, aborting...\e[0m"
-        printf "\n%s\n" "${delimiter}"
-        exit 1
-    fi
+    "${python_cmd}" -m venv "${venv_dir}"
+    source "${venv_dir}"/bin/activate
 else
     printf "\n%s\n" "${delimiter}"
-    printf "python venv already activate or run without venv: ${VIRTUAL_ENV}"
+    printf "Ambiente virtuale %s già attivato" "${venv_dir}"
     printf "\n%s\n" "${delimiter}"
 fi
 
-# Try using TCMalloc on Linux
-prepare_tcmalloc() {
-    if [[ "${OSTYPE}" == "linux"* ]] && [[ -z "${NO_TCMALLOC}" ]] && [[ -z "${LD_PRELOAD}" ]]; then
-        TCMALLOC="$(PATH=/usr/sbin:$PATH ldconfig -p | grep -Po "libtcmalloc(_minimal|)\.so\.\d" | head -n 1)"
-        if [[ ! -z "${TCMALLOC}" ]]; then
-            echo "Using TCMalloc: ${TCMALLOC}"
-            export LD_PRELOAD="${TCMALLOC}"
-        else
-            printf "\e[1m\e[31mCannot locate TCMalloc (improves CPU memory usage)\e[0m\n"
-        fi
-    fi
-}
+# Installa le dipendenze
+printf "\n%s\n" "${delimiter}"
+printf "Installazione delle dipendenze"
+printf "\n%s\n" "${delimiter}"
 
-KEEP_GOING=1
-export SD_WEBUI_RESTART=tmp/restart
-while [[ "$KEEP_GOING" -eq "1" ]]; do
-    if [[ ! -z "${ACCELERATE}" ]] && [ ${ACCELERATE}="True" ] && [ -x "$(command -v accelerate)" ]; then
-        printf "\n%s\n" "${delimiter}"
-        printf "Accelerating launch.py..."
-        printf "\n%s\n" "${delimiter}"
-        prepare_tcmalloc
-        accelerate launch --num_cpu_threads_per_process=6 "${LAUNCH_SCRIPT}" "$@"
-    else
-        printf "\n%s\n" "${delimiter}"
-        printf "Launching launch.py..."
-        printf "\n%s\n" "${delimiter}"
-        prepare_tcmalloc
-        "${python_cmd}" -u "${LAUNCH_SCRIPT}" "$@"
-    fi
+# Se TORCH_COMMAND è definito, installa torch
+if [[ -n "${TORCH_COMMAND}" ]]
+then
+    printf "Esecuzione di %s\n" "${TORCH_COMMAND}"
+    eval "${TORCH_COMMAND}"
+fi
 
-    if [[ ! -f tmp/restart ]]; then
-        KEEP_GOING=0
-    fi
-done
+# Installa il resto delle dipendenze
+pip install -r requirements.txt
+
+# Installazione completata
+printf "\n%s\n" "${delimiter}"
+printf "Installazione completata, puoi avviare l'interfaccia web eseguendo:\n"
+printf "cd %s && bash webui.sh\n" "${clone_dir}"
+printf "\n%s\n" "${delimiter}"
+
+# Esegui lo script di lancio
+if [[ ! -z "${LAUNCH_SCRIPT}" ]]; then
+    printf "\nEsecuzione di %s\n" "${LAUNCH_SCRIPT}"
+    exec "${python_cmd}" "${LAUNCH_SCRIPT}"
+fi
